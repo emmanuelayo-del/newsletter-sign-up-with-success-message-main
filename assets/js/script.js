186a3bfb03d877b229dddabf1f651ba05eb4f8b8
Emmanuel Lawal <ea3333838@gmail.com>
2025-02-18 19:30:49 +0100

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ const btnSubscribe = document.querySelector('.btn-subscribe')
 const valiedEmailEL = document.querySelector('.valid--email')
 const textColor = document.querySelector('.text-color')
 const targetEmail = document.querySelector('.target-email')
+const btnDismiss = document.querySelector('.btn-dismiss')
 
-const successPage = document.querySelector('.success')
+const successPage = document.querySelector('.success');
 const signupPage = document.querySelector('.sign--up')
 
 
 // ADDING EVENT LISTENER TO BUTTON
 btnSubscribe.addEventListener('click', function() {
     const email = emailEL.value;
-
      const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/; // Regular expression for email
    
     //1. Check if user inputs email
@@ -37,4 +37,12 @@ btnSubscribe.addEventListener('click', function() {
          emailEL.classList.add('text-color')
     }
     
+})
+
+btnDismiss.addEventListener('click', ()=> {
+    successPage.classList.add('hidden');
+    signupPage.classList.remove('hidden');
+    valiedEmailEL.classList.add('hidden')
+    emailEL.classList.remove('error')
+    emailEL.value = '';
 })
